7362281d4851f75c6749b3414cdfdc76dbf29f65
Françoise Banier <51740531+fbanier@users.noreply.github.com>
2025-08-27 16:57:38 +0200

    login redirect & login css

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router, RouterLink} from '@angular/router';
+import {ApiServicePublic} from '../../utils/services/api-service-public';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +8,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
-export class Navbar {
+export class Navbar implements OnInit{
+  public isLogged : boolean = false;
+
+  constructor(private apiService: ApiServicePublic) {}
+
+  ngOnInit(): void {
+    if (this.apiService.getToken() != null)
+      this.isLogged = true;
+    }
+
+
 
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { LoginComp } from '../../components/login-comp/login-comp';
 import { Register } from '../../components/register/register';
 @Component({
   selector: 'app-login',
-  imports: [LoginComp,Register],
+  imports: [Register],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
